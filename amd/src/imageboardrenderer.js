d186fe94eedcb52460cc9e7c72532e64d6da4825
Andreas Schenkel <Andreas.Schenkel@web.de>
2023-08-03 00:10:24 +0200

    hide or showhelpergrid on click at backgroundimage

--- amd/src/imageboardrenderer.js
+++ amd/src/imageboardrenderer.js
@@ -3,14 +3,22 @@
  * @param {string} canvaswidth
  */
 export const init = (cmid, canvaswidth) => {
-    window.addEventListener("resize", function() {
+    window.addEventListener("resize", function () {
         if (canvaswidth > getWidth()) {
             let newcanvaswidth = getWidth() * 0.90;
-            let widthfactor =  newcanvaswidth / canvaswidth;
-            const mydiv = document.getElementById("module-" + cmid);
-            mydiv.style.transform="scale("+widthfactor+")";
-            mydiv.style.transformOrigin="0 0";
+            let widthfactor = newcanvaswidth / canvaswidth;
+            const mydiv = document.getElementById("module-"+cmid);
+            mydiv.style.transform = "scale("+widthfactor+")";
+            mydiv.style.transformOrigin = "0 0";
         }
+    });
+
+    window.addEventListener("click", function () {
+        const elements = document.getElementsByClassName("unilabeltype-imageboard-helpergrid-"+cmid);
+        Array.prototype.forEach.call(elements, function (element) {
+            element.classList.toggle("hidden");
+        });
+
     });
 };
 
