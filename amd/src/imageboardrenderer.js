34d7dbc17c33e7b10dd02da3813131de1c9a5d2b
Andreas Schenkel <Andreas.Schenkel@web.de>
2023-09-23 09:08:23 +0200

    do not use dummy as variablename

--- amd/src/imageboardrenderer.js
+++ amd/src/imageboardrenderer.js
@@ -9,14 +9,14 @@ export const init = (cmid, canvaswidth, canvasheight) => {
     const resizer = document.getElementById("unilabeltype-imageboard-resizer-"+cmid);
     resizer.addEventListener("click", function () {
 
-            const dummy = document.getElementById("unilabeltype-imageboard-"+cmid);
+            const imageboard = document.getElementById("unilabeltype-imageboard-"+cmid);
             let newcanvaswidth = 0;
             if (getWidth() < 750) {
-                newcanvaswidth = (getWidth() - dummy.getBoundingClientRect().left)-10;
+                newcanvaswidth = (getWidth() - imageboard.getBoundingClientRect().left)-10;
             } else if (getWidth() < 585) {
-                newcanvaswidth = (getWidth() - dummy.getBoundingClientRect().left)-20;
+                newcanvaswidth = (getWidth() - imageboard.getBoundingClientRect().left)-20;
             } else {
-                newcanvaswidth = (getWidth() - dummy.getBoundingClientRect().left)-80;
+                newcanvaswidth = (getWidth() - imageboard.getBoundingClientRect().left)-80;
             }
 
             if (newcanvaswidth > canvaswidth) {
