669983a69eda4a78b80eb905c755819169324eb9
andreasschenkel <31856043+andreasschenkel@users.noreply.github.com>
2023-10-14 18:17:33 +0200

    Revert "v2.0.1"

--- version.php
+++ version.php
@@ -27,7 +27,6 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->component = 'unilabeltype_imageboard';
-$plugin->version   = 2023100601;
-$plugin->release = 'v2.0.1';
+$plugin->version   = 2023100600;
 $plugin->requires  = 2022111800;
 $plugin->dependencies = ['mod_unilabel' => 2023042304];
