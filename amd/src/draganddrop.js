6905ec913d4eb749c547cb4df88d51c2ed27583b
Andreas Schenkel <Andreas.Schenkel@web.de>
2023-12-20 17:07:25 +0100

    add variable to init-function preview to prevent preview in nonediting mode, add eventlistener to coorinate inputfields to update imageposition

--- amd/src/draganddrop.js
+++ amd/src/draganddrop.js
@@ -1,30 +1,23 @@
 
-export const init = () => {
-    const imageid_1 = document.getElementById("imageid_1");
-    const unilabel_imageboard_image_1 = document.getElementById("unilabel-imageboard-image-1");
-
-    //var width = imageid_1.clientWidth;
-    // Eventlistener an das Inputfeld für die x-Koordinate anhängen
-    var input_xposition_0 = document.getElementById("id_unilabeltype_imageboard_xposition_0");
-    input_xposition_0.addEventListener("focusout", function() {
-        imageid_1.style.left = input_xposition_0.value + "px";
-    });
-    var input_yposition_0 = document.getElementById("id_unilabeltype_imageboard_yposition_0");
-    input_yposition_0.addEventListener("focusout", function() {
-        imageid_1.style.top = input_yposition_0.value + "px";
-    });
-
-    var input_targetwidth_0 = document.getElementById("id_unilabeltype_imageboard_targetwidth_0");
-    input_targetwidth_0.addEventListener("focusout", function() {
-        alert("input_targetwidth_0 " + input_targetwidth_0.value );
-        unilabel_imageboard_image_1.style.width = input_targetwidth_0.value + "px";
-    });
-
-    var input_targetheight_0 = document.getElementById("id_unilabeltype_imageboard_targetheight_0");
-    input_targetheight_0.addEventListener("focusout", function() {
-        alert("input_targetheight_0 " + input_targetheight_0.value );
-        unilabel_imageboard_image_1.style.height = "50px";
-    });
-
-
+export const init = (ispreviewmode) => {
+    if (ispreviewmode == 0) {
+        // We do not need eventlisteners because we are not in editing mode where we need preview.
+        return;
+    }
+    const imageids = document.querySelectorAll('[id^="imageid_"]');
+    let i = 0;
+    for (i=0; i<imageids.length; i++) {
+        let imageid = imageids[i];
+        // Eventlistener an das Inputfeld für die x-Koordinate anhängen
+        const input_xposition = document.getElementById("id_unilabeltype_imageboard_xposition_" + (i));
+        input_xposition.addEventListener("focusout", function() {
+            const x = parseInt(input_xposition.value);
+            imageid.style.left = x + "px";
+        });
+        const input_yposition = document.getElementById("id_unilabeltype_imageboard_yposition_" + (i));
+        input_yposition.addEventListener("focusout", function() {
+            const y = parseInt(input_yposition.value);
+            imageid.style.top = y + "px";
+        });
+    }
 };
